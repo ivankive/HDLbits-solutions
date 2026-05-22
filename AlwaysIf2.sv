0135d5d41f71a3c582248108644b471aff35d91a
Ivan Kivelevitch <ivankivelevitch@gmail.com>
2026-05-22 21:01:59 +0000

    w

---
+++ AlwaysIf2.sv
@@ -0,0 +1,34 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        if (cpu_overheated)
+           shut_off_computer = 1;
+        else begin
+            shut_off_computer = 0;
+        end
+    end
+
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+    
+        else if (arrived && ~gas_tank_empty) begin
+        
+        keep_driving = gas_tank_empty;
+        
+            end else begin
+                
+                keep_driving = ~gas_tank_empty;    
+                
+            
+    	end
+    end
+    
+
+endmodule
